43a909db50f7e8ed287521b3aaa22d0ba8e6daa6
EricLim73 <ljheric7@gmail.com>
2022-09-06 16:32:44 +0900

    additional comment explaining shit

--- src/Render/camera.cpp
+++ src/Render/camera.cpp
@@ -34,13 +34,14 @@ void Camera::setPerspectiveProjection(float fov, float aspect, float near, float
     projectionMatrix[2][2] = far / (far - near);
     projectionMatrix[2][3] = 1.f;
     projectionMatrix[3][2] = -(far * near) / (far - near);
-}
+} 
 
 void Camera::setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up) {
+    //  Setting Orthonormal Basis -> use this as base for rotation + multiply with position matrix to form the whole transform
     //  3 unit vector can describe 1 coordinate space
-    const glm::vec3 w{glm::normalize(direction)};   //  camera's view direction unit
-    const glm::vec3 u{glm::normalize(glm::cross(w, up))};   //  camera's right direction unit
-    const glm::vec3 v{glm::cross(w, u)};    //  camera's up direction unit
+    const glm::vec3 w{glm::normalize(direction)};   //  camera's view direction unit            [z]
+    const glm::vec3 u{glm::normalize(glm::cross(w, up))};   //  camera's right direction unit   [x]
+    const glm::vec3 v{glm::cross(w, u)};    //  camera's up direction unit                      [y]
     viewMatrix = glm::mat4{1.f};
     viewMatrix[0][0] = u.x;
     viewMatrix[1][0] = u.y;
@@ -57,11 +58,15 @@ void Camera::setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3
 }
 
 void Camera::setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up) {
-    //assert((target-position).length() == 0);
-    setViewDirection(position, target - position, up);
+    auto direction = target - position;
+    if(glm::dot(direction, direction) > std::numeric_limits<float>::epsilon()){
+        setViewDirection(position, target - position, up);
+        return;
+    }
+    setViewDirection(position, target, up);
 }
 
-//  camera movement & rotation -> Trait-Bryan Euler Angle YXZ
+//  camera movement & rotation -> Trait-Bryan Euler Angle YXZ ( rotation vector in this context has all uvw )
 void Camera::setViewYXZ(glm::vec3 position, glm::vec3 rotation) {
     const float c3 = glm::cos(rotation.z);
     const float s3 = glm::sin(rotation.z);
@@ -85,6 +90,13 @@ void Camera::setViewYXZ(glm::vec3 position, glm::vec3 rotation) {
     viewMatrix[3][0] = -glm::dot(u, position);
     viewMatrix[3][1] = -glm::dot(v, position);
     viewMatrix[3][2] = -glm::dot(w, position);
+    /*  -> rotation matrix is inversed so result is the world rotating according to the camera space origin 
+        ->  "-" position bc now we are looking in camera space, world should move opposite amount from origin to match
+        ux uy uz 0      1 0 0 px
+        vx vy vz 0  *   0 1 0 py
+        wx wy wz 0      0 0 1 pz
+        0  0  0  1      0 0 0  1
+    */
 }
 
 }
